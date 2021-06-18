8c8e32ea749cb77a5b6664128e3539ec0785df7c
yonaskolb <yonaskolb@gmail.com>
2021-06-18 14:44:31 +1000

    point to zip

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Instabug",
-            url: "https://ios-releases.instabug.com/release/10.7.1/archive.zip",
-            checksum: "59beffda10aa5eacdfec01d8a8c84cbed9530d4cac0f706a48a9582a4f8c0c67")
+            url: "https://github.com/yonaskolb/Instabug-SP/blob/master/Instabug.xcframework.zip?raw=true",
+            checksum: "9d531e568df1be4511a8dabcdd0406e08a5a8911095c48980ae4ebded817df7c")
     ]
 )
